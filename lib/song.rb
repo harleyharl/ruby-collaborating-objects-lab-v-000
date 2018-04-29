9fba3745a0d7c3ab80d7ba6e761dfff5f5d9667f
Harley Rayner <harleymteden@gmail.com>
2018-04-29 23:57:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,19 @@
 class Song
-  
+
   attr_reader :artist, :name
 
   def initialize(name)
     @name = name
     @artist = nil
+  end
+
+  def artist=(artist)
+    @artist = artist
+    
+  end
+
+  def self.new_from_filename(filename)
+
   end
 
 end
