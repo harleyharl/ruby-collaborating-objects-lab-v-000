4107a66563c16b7480727733aca2f1825502c520
Harley Rayner <harleymteden@gmail.com>
2018-04-29 23:35:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,12 +14,26 @@ class Artist
     @@all
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+  def self.create_by_name(name)
+
+  end
+
   def add_song(song)
     @songs << song
   end
 
   def save
     @@all.push(self)
+    #the thing we just saved now gets returned instead of the whole array
+    self
   end
 
 end
